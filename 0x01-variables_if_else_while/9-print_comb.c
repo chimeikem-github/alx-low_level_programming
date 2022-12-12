c81678eb6b3e4e1617e31d07e66e14fc75e16136
chimeikem-github <collins.elom47@gmail.com>
2022-12-12 14:05:58 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ int u = 48;
+int p = 44;
+
+while (u <= 57)
+{ putchar(u);
+if (u != 57)
+{
+	putchar(p);
+	putchar(32);
+}
+n++;
+}
+putchar('\n');
+return (0);
+}
