98e06ec2257d8d1f6e0926ab7f18ee5d266ffe9f
chimeikem-github <collins.elom47@gmail.com>
2022-12-12 12:13:09 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m = 0;
+
+	while (n < 10)
+	{
+	putchar("%d \n", m);
+	m += 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
