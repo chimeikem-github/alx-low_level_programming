abd875aa08ccbf31d0b7a7b752a0e2169d5572ca
chimeikem-github <collins.elom47@gmail.com>
2022-12-12 05:45:36 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ if ((n % 10) > 5)
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
 }
 else
 {
