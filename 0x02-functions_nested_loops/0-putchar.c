b6d4bd1f139d1aea3b3ac39eeeed98b8270f5365
chimeikem-github <collins.elom47@gmail.com>
2022-12-13 04:47:59 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -E ntry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char z[] = " ALX SE";
+int m;
+m = 0;
+while (m <= 8)
+{
+	_putchar(z[m]);
+	m++;
+}
+_putchar('\n');
+return (0);
+}
