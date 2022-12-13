14eac2d578f8d95568f59ad17c0a612c8bf955b9
chimeikem-github <collins.elom47@gmail.com>
2022-12-12 20:43:28 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int d1 = 48;
+	int d2 = 48;
+	int d3 = 44;
+
+	while (d1 <= 57)
+	{
+	n2 = 48;
+	if (n2 <= 57)
+	{
+	putchar(d1);
+	putchar(d2);
+
+	if (d1 != 57 || n2 != 57)
+	{
+	putchar(d3);
+	putchar(32);
+	}
+	d2++;
+	}
+	d1++
+	}
+	putchar('\n');
+
+	return (0);
+}
