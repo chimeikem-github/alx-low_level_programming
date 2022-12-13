e5e5ca911fa69aee239461e587fa131bd6371a68
chimeikem-github <collins.elom47@gmail.com>
2022-12-13 12:30:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int m;
+for (m = 'a'; m <= 'z'; m++)
+{
+	_putchar(m);
+}
+return (0);
+}
