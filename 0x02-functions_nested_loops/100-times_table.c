a05388ee81febfe5f9bbfe396e68d71dc6c2b3e7
chimeikem-github <collins.elom47@gmail.com>
2022-12-14 02:04:32 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
-{	_print_times_table(3);
+{	 print_times_table(3);
 	_putchar('\n');
-	_print_times_table(5);
+	 print_times_table(5);
 	_putchar('\n');
-	_print_times_table(98);
+	 print_times_table(98);
 	_putchar('\n');
-	_print_times_table(12);
+	 print_times_table(12);
 	return (0);
 }
