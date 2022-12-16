60a73b8cc29e7f20632572fff4f8a18eefc4237f
chimeikem-github <collins.elom47@gmail.com>
2022-12-16 16:25:44 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - function that print all numbers from
+ *
+ * Return: returns nothing
+ */
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
