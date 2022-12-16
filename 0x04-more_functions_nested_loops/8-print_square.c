948cb94b9d1b8362376e53ef98cb6bbad73d3c0d
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2022-12-16 20:31:50 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*print_diagonal - prints a diagonal
-*@n: parameter
-*Return: returns nothing
+*  print_square - a function that prints a square, followed by a new line
+*  @size: size of both width and length
+*  Return: a square made of '#'
 */
 void print_square(int size)
 {
