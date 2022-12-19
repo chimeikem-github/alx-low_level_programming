f3cf21f156f0a53fee6dfb439566553a5adc3bac
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2022-12-19 15:20:45 +0000

    committed

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-* main - header file for protoypes void protoypes (void) int protoypes (int)
-*/
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
