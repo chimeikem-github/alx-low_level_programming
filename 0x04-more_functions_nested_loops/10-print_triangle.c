90637eea786d94e6da5ff10166089fe34e1ca939
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2022-12-22 06:03:36 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,49 +1,31 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: random integer
- *Return: void
- **/
-
-void print_number(int n)
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
 {
-	char ld, cr;
-	int r;
-	int ct = 0;
-
-	if (n < 0)
+	if (size <= 0)
 	{
-		_putchar ('-');
-		ld = (char)('0' - (n % 10));
-		n = n / -10;
-	}
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
-		else
+		for (i = 1; i <= size; i++)
 		{
-			ld = (char)((n % 10) + '0');
-		n = n / 10;
-	}
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
 
-	r = 0;
-	while (n > 0)
-	{
-		r = r * 10 + (n % 10);
-		n = n / 10;
-		ct++;
-	}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
 
-	while (r > 0)
-	{
-		cr = (char)((r % 10) + '0');
-		_putchar(cr);
-		r = r / 10;
-		ct--;
+			_putchar('\n');
+		}
 	}
-	while (ct != 0)
-	{
-		_putchar('0');
-			ct--;
-	}
-	_putchar(ld);
 }
