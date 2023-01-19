440d6d7d9e6cb5311cb330c2d7b09e6ecd23a84b
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2023-01-19 16:02:14 +0000

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,28 +1,35 @@
 #include "variadic_functions.h"
 /**
- * print_numbers - print all the parametes.
+ * print_strings - print strings.
 (* a blank line
  *@separator: the number of parameters
- *@n: the number of parameters
+ *@n: the number of strings
 * Description: this function prints all the parameters)?
 (* section header: the header of this function is variadic_functions.h)*
 * Return: no retunr.
 */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list num;
 	unsigned int i;
+	va_list strings;
+	char *string;
+
+	va_start(strings, n);
 
 	i = 0;
-
-	va_start(num, n);
 	while (i < n)
 	{
-		printf("%d", va_arg(num, unsigned int));
-		if (i < (n - 1) && separator != NULL)
+		string = va_arg(strings, char*);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 		i++;
 	}
-	va_end(num);
 	printf("\n");
+
+	va_end(strings);
 }
+
