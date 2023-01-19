f7c8a3a49324366a1d89bd0af89faeb1f594f776
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2023-01-19 15:54:11 +0000

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct args_t - contain the type and the function.
+ * @format: the format to print
+ * @function: the functions that prints
+ *
+ * Description: contains the type and the format to print.
+ */
+typedef struct args_t
+{
+	char *format;
+	void (*function)();
+} args_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
