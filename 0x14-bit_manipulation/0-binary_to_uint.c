833c3b33457504856dd3290202a7660c388dd6ab
Cornelius M. Elom <73221031+chimeikem-github@users.noreply.github.com>
2023-02-02 14:24:23 +0000

    corrected

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,37 @@
-/*
- * Auth: Brennan D Baraban
- * File: 0-binary_to_uint.c
- */
-
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts binary number to an unsigned int
+ * @b: a string of 0 and 1 chars
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: converted number or 0 if b is NULL or different from 0 or 1
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	int length, i, add;
 
-	if (b == '\0')
-		return (0);
+	add = 0;
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	if (b != NULL)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		for (length = 0 ; b[length] != '\0' ; length++)
+		{
+			if (b[length] != 48 && b[length] != 49)
+			{
+				return (0);
+			}
+		}
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		for (i = 0, length-- ; length >= 0 ; length--)
+		{
+			add = add + ((b[length] - '0') << i);
+			i++;
+		}
 	}
-
-	return (num);
+	else
+	{
+		return (0);
+	}
+	return (add);
 }
-
